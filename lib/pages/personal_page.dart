23644a2c16b22f0aeaeaac82f887bcf9c5ca1d00
EthanAdkins <102610253+EthanAdkins@users.noreply.github.com>
2023-03-25 19:08:30 -0400

    Update personal_page.dart

--- lib/pages/personal_page.dart
+++ lib/pages/personal_page.dart
@@ -18,64 +18,113 @@ class PersonalPage extends StatefulWidget {
 }
 
 class _PersonalPageState extends State<PersonalPage> {
+  final List<String> entries = <String>[
+    'A',
+    'B',
+    'C',
+    'A',
+    'B',
+    'C',
+    'A',
+    'B',
+    'C',
+    'A',
+    'B',
+    'C'
+  ];
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Color(0xfff2f1e8),
       drawer: new DrawerCodeOnly(),
       appBar: new AppBar(
           title: new Text("Personal Page"),
           backgroundColor: Color(0xffa5b2df),
           iconTheme: IconThemeData(color: Color(0xff5a4fcf))),
-      body: Column(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height / 6,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Align(
-                  alignment: Alignment.center,
-                  child: Image.asset(
-                    "assets/images/ToDoIcon.png",
-                    width: 300,
-                    fit: BoxFit.contain,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 6,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.center,
+                    child: Image.asset(
+                      "assets/images/ToDoIcon.png",
+                      width: 300,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 25),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xff9736c5),
+                  textStyle: TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, "/addTask");
+                },
+                child: const Text('Create New Task'),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(240, 245, 249, 255),
+                border: Border.all(
+                  width: 1,
+                  color: Color.fromARGB(255, 71, 71, 71),
+                ),
+                borderRadius: BorderRadius.circular(3),
+              ),
+              /*child: LimitedBox(
+                maxHeight: 300,
+                maxWidth: 355,
+                child: Column(
+                  children: [],
+                ),
+              ),*/
+
+              /*
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: 50, minWidth: 355),
+                child: Column(
+                  children: [
+                    
+                  ],
+              */
+
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: 50, minWidth: 355),
+                child: Expanded(
+                  child: ListView.separated(
+                    physics: ClampingScrollPhysics(),
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    padding: const EdgeInsets.all(10),
+                    itemCount: entries.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        height: 70,
+                        color: Colors.purple,
+                        child: Center(child: Text('Entry ${entries[index]}')),
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) =>
+                        const Divider(),
                   ),
                 ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 25),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xff9736c5),
-                textStyle: TextStyle(fontSize: 20),
-              ),
-              onPressed: () {
-                Navigator.pushNamed(context, "/addTask");
-              },
-              child: const Text('Create New Task'),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: Color.fromARGB(240, 245, 249, 255),
-              border: Border.all(
-                width: 1,
-                color: Color.fromARGB(255, 71, 71, 71),
-              ),
-              borderRadius: BorderRadius.circular(3),
-            ),
-            child: ConstrainedBox(
-              constraints: BoxConstraints(minHeight: 50, minWidth: 355),
-              child: Column(
-                children: [],
               ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
