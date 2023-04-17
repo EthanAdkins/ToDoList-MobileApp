ee1b28d3b082157d552ec28a822a713468d302f7
EthanAdkins <102610253+EthanAdkins@users.noreply.github.com>
2023-04-17 01:43:51 -0400

    Added Search Functionality

--- lib/pages/personal_page.dart
+++ lib/pages/personal_page.dart
@@ -24,7 +24,8 @@ class PersonalPage extends StatefulWidget {
 }
 
 class _PersonalPageState extends State<PersonalPage> {
-  final List<String> entries = <String>[
+  int count = 5;
+  static List<String> entries = <String>[
     'I have a test due and must get my chores done/wash the Testing',
     'Test Task',
     'The fitness gram pacer test is a ... nevermind',
@@ -38,16 +39,35 @@ class _PersonalPageState extends State<PersonalPage> {
     'B',
     'C'
   ];
+  List<String> changeableList = List.from(entries);
+
   GlobalKey<FormState> globalFormKey = GlobalKey<FormState>();
   String searchValue = '';
+
+  bool isAPIcallProcess = false;
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return SafeArea(
+        child: Scaffold(
+            backgroundColor: HexColor("#A5B2DF"),
+            body: ProgressHUD(
+              child: Form(
+                key: globalFormKey,
+                child: _personalUI(context),
+              ),
+              inAsyncCall: isAPIcallProcess,
+              opacity: 0.3,
+              key: UniqueKey(),
+            )));
+  }
+
+  Widget _personalUI(BuildContext context) {
+    return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Color(0xfff2f1e8),
-      drawer: new DrawerCodeOnly(),
-      appBar: new AppBar(
-          title: new Text("Personal Page"),
+      drawer: DrawerCodeOnly(),
+      appBar: AppBar(
+          title: Text("Personal Page"),
           backgroundColor: Color(0xffa5b2df),
           iconTheme: IconThemeData(color: Color(0xff5a4fcf))),
       body: SingleChildScrollView(
@@ -85,36 +105,84 @@ class _PersonalPageState extends State<PersonalPage> {
             ),
             Padding(
               padding: const EdgeInsets.only(bottom: 25),
-              child: FormHelper.inputFieldWidget(
-                context,
-                'search',
-                'Search',
-                (onValidateVal) {
-                  return null;
-                },
-                (onSavedVal) {
-                  searchValue = onSavedVal;
-                },
-                prefixIcon: const Icon(Icons.search),
-                showPrefixIcon: true,
-                borderFocusColor: Color(0xff9736c5),
-                prefixIconColor: Color(0xff9736c5),
-                borderColor: Color(0xff9736c5),
-                textColor: Color(0xff9736c5),
-                hintColor: Color(0xff9736c5).withOpacity(0.7),
-                borderRadius: 10,
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 3,
+                    child: FormHelper.inputFieldWidget(
+                      context,
+                      'search',
+                      'Search',
+                      (onValidateVal) {
+                        return null;
+                      },
+                      (onSavedVal) {
+                        searchValue = onSavedVal;
+                      },
+                      paddingRight: 0,
+                      prefixIcon: const Icon(Icons.search),
+                      showPrefixIcon: true,
+                      borderFocusColor: Color(0xff9736c5),
+                      prefixIconColor: Color(0xff9736c5),
+                      borderColor: Color(0xff9736c5),
+                      textColor: Color(0xff9736c5),
+                      hintColor: Color(0xff9736c5).withOpacity(0.7),
+                      borderRadius: 10,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: FormHelper.submitButton(
+                      "SEARCH",
+                      () {
+                        if (validateAndSave()) {
+                          List<String> matchQuery = [];
+                          for (var entry in entries) {
+                            if (entry
+                                .toLowerCase()
+                                .contains(searchValue.toLowerCase())) {
+                              matchQuery.add(entry);
+                              // This is inefficient by a lot. May remove
+                            }
+                          }
+
+                          changeableList = matchQuery;
+                          setState(() {
+                            count++;
+                          });
+                          PersonalPage(key: ValueKey(count));
+                        } else {
+                          print('huh');
+                        }
+                      },
+                      btnColor: HexColor("#9736C5"),
+                      borderColor: Colors.white,
+                      txtColor: Colors.white,
+                      borderRadius: 10,
+                      width: 80,
+                    ),
+                  ),
+                ],
               ),
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: Color.fromARGB(240, 245, 249, 255),
-                border: Border.all(
-                  width: 1,
-                  color: Color.fromARGB(255, 71, 71, 71),
-                ),
-                borderRadius: BorderRadius.circular(3),
-              ),
-              /*child: LimitedBox(
+            _displayUI(context)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _displayUI(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(240, 245, 249, 255),
+        border: Border.all(
+          width: 1,
+          color: Color.fromARGB(255, 71, 71, 71),
+        ),
+        borderRadius: BorderRadius.circular(3),
+      ),
+      /*child: LimitedBox(
                 maxHeight: 300,
                 maxWidth: 355,
                 child: Column(
@@ -122,7 +190,7 @@ class _PersonalPageState extends State<PersonalPage> {
                 ),
               ),*/
 
-              /*
+      /*
               child: ConstrainedBox(
                 constraints: BoxConstraints(minHeight: 50, minWidth: 355),
                 child: Column(
@@ -131,126 +199,117 @@ class _PersonalPageState extends State<PersonalPage> {
                   ],
               */
 
-              child: ConstrainedBox(
-                constraints: BoxConstraints(minHeight: 50, minWidth: 355),
-                child: ListView.separated(
-                  physics: ClampingScrollPhysics(),
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  padding: const EdgeInsets.all(10),
-                  itemCount: entries.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      height: 70,
-                      color: Color(0xff9736c5),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Center(
-                                child: Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                    top: 10,
-                                    bottom: 3,
-                                    left: 15,
-                                  ),
-                                  child: AutoSizeText(
-                                    'Entry ${entries[index]}',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    ),
-                                    maxLines: 3,
-                                    minFontSize: 8,
-                                    maxFontSize: 30,
-                                  ),
-                                ),
-                                AutoSizeText(
-                                  'Due Date: 4/12/23 4:23 PM',
-                                  style: TextStyle(
-                                    decoration: TextDecoration.underline,
-                                    fontSize: 10,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ],
-                            )),
+      child: ConstrainedBox(
+        constraints: BoxConstraints(minHeight: 50, minWidth: 355),
+        child: ListView.separated(
+          physics: ClampingScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(10),
+          itemCount: changeableList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 70,
+              color: Color(0xff9736c5),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Center(
+                        child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 10,
+                            bottom: 3,
+                            left: 15,
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 10, left: 10),
-                            child: SizedBox(
-                              width: 40,
-                              height: 40,
-                              child: ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor:
-                                        Color.fromARGB(255, 115, 41, 150),
-                                    textStyle: TextStyle(fontSize: 15),
-                                    padding: EdgeInsets.zero,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                  ),
-                                  onPressed: () async {
-                                    // Check here if the date is in the future. Message if now
-                                    //if (date > or whatever DateTime now = new DateTime.now(); )
-                                    if (await confirm(
-                                      context,
-                                      content: const Text(
-                                          'Would you like to be reminded when your task is due?'),
-                                    )) {
-                                      DateTime dateTimeTEST = dateFormat
-                                          .parse("2023-04-13 00:56:00");
+                          child: AutoSizeText(
+                            'Entry ${changeableList[index]}',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                            maxLines: 3,
+                            minFontSize: 8,
+                            maxFontSize: 30,
+                          ),
+                        ),
+                        AutoSizeText(
+                          'Due Date: 4/12/23 4:23 PM',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 10,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10, left: 10),
+                    child: SizedBox(
+                      width: 40,
+                      height: 40,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromARGB(255, 115, 41, 150),
+                            textStyle: TextStyle(fontSize: 15),
+                            padding: EdgeInsets.zero,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          onPressed: () async {
+                            // Check here if the date is in the future. Message if now
+                            //if (date > or whatever DateTime now = new DateTime.now(); )
+                            if (await confirm(
+                              context,
+                              content: const Text(
+                                  'Would you like to be reminded when your task is due?'),
+                            )) {
+                              DateTime dateTimeTEST =
+                                  dateFormat.parse("2023-04-13 00:56:00");
 
-                                      NotificationService()
-                                          .scheduleNotification(
-                                              title: 'Wow',
-                                              body: 'That worked',
-                                              scheduledNotificationDateTime:
-                                                  dateTimeTEST);
-                                    } else {
-                                      return print("not confirm");
-                                    }
-                                  },
-                                  child: const Icon(Icons.access_time_outlined,
-                                      size: 20)),
-                            ),
+                              NotificationService().scheduleNotification(
+                                  title: 'Wow',
+                                  body: 'That worked',
+                                  scheduledNotificationDateTime: dateTimeTEST);
+                            } else {
+                              return print("not confirm");
+                            }
+                          },
+                          child:
+                              const Icon(Icons.access_time_outlined, size: 20)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: SizedBox(
+                      width: 55,
+                      height: 40,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromARGB(255, 115, 41, 150),
+                            textStyle: TextStyle(fontSize: 15),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 10),
-                            child: SizedBox(
-                              width: 55,
-                              height: 40,
-                              child: ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor:
-                                        Color.fromARGB(255, 115, 41, 150),
-                                    textStyle: TextStyle(fontSize: 15),
-                                  ),
-                                  onPressed: () async {
-                                    if (await confirm(
-                                      context,
-                                      content:
-                                          const Text('Confirm delete task?'),
-                                      textOK: const Text('Delete'),
-                                    )) {
-                                      return print("confirm");
-                                    }
-                                    return print("not confirm");
-                                  },
-                                  child: const Icon(Icons.delete, size: 20)),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) =>
-                      const Divider(),
-                ),
+                          onPressed: () async {
+                            if (await confirm(
+                              context,
+                              content: const Text('Confirm delete task?'),
+                              textOK: const Text('Delete'),
+                            )) {
+                              return print("confirm");
+                            }
+                            return print("not confirm");
+                          },
+                          child: const Icon(Icons.delete, size: 20)),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
         ),
       ),
     );
